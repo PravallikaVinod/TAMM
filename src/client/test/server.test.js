8a717fd47b2d707456cc8545ee6a52e1107a4818
Pravallika <gunjapalle.pravallika@gmail.com>
2020-01-13 15:36:44 +0530

    Update server.test.js

--- src/client/test/server.test.js
+++ src/client/test/server.test.js
@@ -1,4 +1,5 @@
 
+
 const app = require("../../index.js");
 const chai = require("chai");
 const chaiHttp = require("chai-http");
@@ -23,6 +24,18 @@ describe("Server!", () => {
       .get("/api/creditcard/list")
       .end((err, res) => {
         expect(res).to.have.status(200);
+        done();
+      });
+  });
+
+  it("Add New Credit card to the JSON file", done => {
+    chai
+      .request(app)
+      .post("/api/creditcard/add")
+      .send({"name":"Sita","cardNumber":79927398713,"limit":100000})
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body.status).to.equals("Card saved successfully");
         done();
       });
   });
